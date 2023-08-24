d3a8260afb4056fd2ad669358bcd9a45a15f6d5d
Dominic <git@msrd0.de>
2023-08-24 18:41:36 +0200

    fix database implementation

--- src/mxbot/db.rs
+++ src/mxbot/db.rs
@@ -1,29 +1,38 @@
-use super::state::{read_media_map, write_media_map, MediaMap};
+use super::state::{read_media_map, write_media_map, MediaCache, MediaHash, MediaMap};
 use matrix_sdk::Client;
 use mstickerlib::database::{self, Database};
+use parking_lot::RwLock;
+use std::sync::Arc;
 
 #[must_use]
 pub struct AccountDataDatabase {
-	map: MediaMap
+	map: Arc<RwLock<MediaMap>>
 }
 
 impl AccountDataDatabase {
 	async fn load(client: &Client) -> anyhow::Result<Self> {
 		let map = read_media_map(client).await?.unwrap_or_default();
-		Ok(Self { map })
+		Ok(Self {
+			map: Arc::new(RwLock::new(map))
+		})
 	}
 
 	async fn store(self, client: &Client) -> anyhow::Result<()> {
-		write_media_map(client, &self.map).await
+		// TODO fix non-async RwLock
+		let map = self.map.read();
+		write_media_map(client, &map).await
 	}
 }
 
 impl Database for AccountDataDatabase {
 	fn get(&self, hash: &database::Hash) -> Option<String> {
-		self.map.map.get(hash).map(|cache| cache.url.clone())
+		let map = self.map.read();
+		map.map.get(hash).map(|cache| cache.url.clone())
 	}
 
 	fn add(&self, hash: database::Hash, url: String) -> anyhow::Result<()> {
-		unimplemented!()
+		let mut map = self.map.write();
+		map.map.insert(MediaHash(hash), MediaCache { url });
+		Ok(())
 	}
 }
